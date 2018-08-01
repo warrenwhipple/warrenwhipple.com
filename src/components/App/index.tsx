9b2f2ca10a859053d8d2c46e436d9915b575eb41
Warren Whipple <modalrealist@gmail.com>
2018-08-01 09:00:30 -0400

    Clean and reorganize App component

---
+++ src/components/App/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        Hellow world!
+      </div>
+    );
+  }
+}
+
+export default App;
