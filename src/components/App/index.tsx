d8dc41b5b9e5675555f6e104c5fe7ba96b46e1c5
Warren Whipple <modalrealist@gmail.com>
2018-08-01 09:38:52 -0400

    Add Prettier

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,11 +2,7 @@ import * as React from 'react';
 
 class App extends React.Component {
   public render() {
-    return (
-      <div className="App">
-        Hellow world!
-      </div>
-    );
+    return <div className="App">Hellow world!</div>;
   }
 }
 
