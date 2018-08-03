915f250ed8a253f9f3e487f17e700f6a06ab9118
Warren Whipple <modalrealist@gmail.com>
2018-08-03 12:37:47 -0400

    Add App enzyme test

--- src/components/App/index.test.tsx
+++ src/components/App/index.test.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders a div', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('div').length).toEqual(1);
 });
