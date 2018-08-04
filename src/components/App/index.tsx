356d1fc829d6a3948bcab10c0bae072cae1d6852
Warren Whipple <modalrealist@gmail.com>
2018-08-04 12:10:31 -0400

    Add Landing component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import Landing from '../Landing';
 import PrimaryHeader from '../PrimaryHeader';
 
 const App = () => (
   <div className="app">
     <PrimaryHeader />
-    <div className="content">Content</div>
+    <div className="content">
+      <Landing />
+    </div>
   </div>
 );
 
---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+const Landing = () => <div className="Landing">Landing content.</div>;
+
+export default Landing;
